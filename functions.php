4582ee1319343cc43cf140f4e364f939a4ba91ea
sophie-senftleben <develop@sophie-senftleben.de>
2018-03-07 16:41:36 +0100

    *refactored functions  *added template support for custom headers

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-
+// Include style sheet and script files
 function load_scripts(){
 	wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
 
@@ -8,12 +8,25 @@ function load_scripts(){
 }
 add_action( 'wp_enqueue_scripts', 'load_scripts' );
 
-register_nav_menus(
-	array(
-		'top_menu' => 'Top Menu',
-		'main_menu' => 'Main Menu',
-		'footer_menu' => 'Footer Menu'
-	)
-);
-
+// Load Bootstrap-NavWalker extension to use Bootstrap menu style
 require get_template_directory() . '/bootstrap-navwalker.php';
+
+// Main configuration
+function configure(){
+	// Register menus
+	register_nav_menus(
+		array(
+			'top_menu'	  => 'Top Menu',
+			'main_menu'   => 'Main Menu',
+			'footer_menu' => 'Footer Menu'
+		)
+	);
+
+	// Add theme support
+	$args = array(
+		'height' => 225,
+		'width'  => 1920
+	);
+	add_theme_support( 'custom-header', $args );
+}
+add_action( 'after_setup_theme', 'configure', 0 );
