b5235e8c12305ab4475ed3bbbdf39fdd22086c2f
sophie-senftleben <develop@sophie-senftleben.de>
2018-03-23 21:23:31 +0100

    added under construction notice

--- front-page.php
+++ front-page.php
@@ -2,8 +2,10 @@
 
 <?php $backgroundImage = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
 
+<div id="under-construction" class="text-center card-img-overlay" style="position: relative; color: darkred;">Ich überarbeite meine Webseite gerade. Die Links stehen bald wieder zur Verfügung.</div>
+
 <main>
-	<div class="^jumbotron parallax parallax-window" data-parallax="scroll" data-image-src="<?php echo $backgroundImage[0]; ?>">
+	<div class="parallax parallax-window" data-parallax="scroll" data-image-src="<?php echo $backgroundImage[0]; ?>">
 		<p class="lead"><?php echo esc_attr( get_option( 'custom_general_settings' )['typewriter_pretext'] ); ?></p>
 		<h1 class="display-3 text-uppercase"><strong><?php echo esc_attr( get_option( 'blogname' ) ); ?></strong></h1>
 		<p class="lead"><?php echo esc_attr( get_option( 'custom_general_settings' )['typewriter_static_text'] ); ?>
